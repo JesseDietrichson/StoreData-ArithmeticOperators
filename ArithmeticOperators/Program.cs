e71b9e75a7060f1db67e9cc446ae8268c1251b68
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-10 22:23:27 -0500

    Add project files.

---
+++ ArithmeticOperators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5 + 5;
+            myInt = 5 * 5;
+            myInt = 5 / 5;
+            myInt = 5 - 5;
+
+
+            int test = 10 / 3;
+            Console.WriteLine(test);
+
+            double test2 = 10.0 / 3.0;
+            Console.WriteLine(test2);
+
+            int remainder = 10 % 3;
+            Console.WriteLine(remainder);
+
+        }
+    }
+}
